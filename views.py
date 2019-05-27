36cae0e158baf62bdc59e00148b0150e984b9e7d
githubphp <33368160+githubphp@users.noreply.github.com>
2019-05-27 11:43:49 +0600

    Update views.py

--- views.py
+++ views.py
@@ -24,24 +24,3 @@ def home(request):
 
 def about(request):
     return render(request, 'polls/about.html', {'title': 'About'})
-
-
-
-
-
-
-
-#from django.http import HttpResponse
-
-#def index(request):
- #   return HttpResponse ("You are in polls Index.")
-
-#def detail(request, question_id):
- #   return HttpResponse("You're looking at question %s." % question_id)
-
-#def results(request, question_id):
- #   response = "You're looking at the results of question %s."
-  #  return HttpResponse(response % question_id)
-
-#def vote(request, question_id):
- #   return HttpResponse("You're voting on question %s." % question_id)
