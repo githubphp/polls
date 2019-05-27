18b893d7355f7e22bc75d86dfbc5ba244e3ec74e
githubphp <33368160+githubphp@users.noreply.github.com>
2019-05-27 12:23:01 +0600

    Update views.py

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ posts = [
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'August 27, 2018'
-    }
+    },
     {
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
